95eb029e067e5d0644da25f0035b473ea1741aee
vivek <vivekchavan382@gmail.com>
2024-03-10 04:33:38 +0530

    created client template

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
 const app = express()
+dotenv.config()
 const port = process.env.PORT || 5000
+
 
 app.get('/',(req,res)=>{
     res.send("Hello")
+})
+
+app.get('/signup',(req,res)=>{
+    res.send("This is signu route")
 })
 
 app.listen(port, () => {
