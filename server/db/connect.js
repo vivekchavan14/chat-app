13a71abb95b094fdafc8230d1fd8cd561bed6c6d
vivek <vivekchavan382@gmail.com>
2024-03-11 03:21:42 +0530

    create auth routes && controllers

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,8 +2,11 @@ import mongoose from 'mongoose';
 
 const connect = async(req,res) => {
       try {
-        
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connected successfully")
       } catch (error) {
-  console.log('Error connecting to MongoDB')
+  console.log('Error connecting to MongoDB',error.message)
       }
 }
+
+export default connect
--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import connect from './db/connect.js'
 const app = express()
 dotenv.config()
 const port = process.env.PORT || 5000
 
-
-app.get('/',(req,res)=>{
-    res.send("Hello")
-})
-
+app.use(express.json())
 app.use('/api/auth',authRoutes)
 
 app.listen(port, () => {
+    connect()
     console.log(`Server is running on http://localhost:${port}`)
 })
