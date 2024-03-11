92043db88a13273f8a06831932bcb89ede71575b
vivek <vivekchavan382@gmail.com>
2024-03-12 00:44:10 +0530

    created message models

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
 import connect from './db/connect.js'
 const app = express()
 dotenv.config()
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use('/api/auth',authRoutes)
+app.use('/api/message', messageRoutes)
 
 app.listen(port, () => {
     connect()
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -24,6 +24,8 @@ const userSchema = new mongoose.Schema({
         type:String,
         default:"",
     }
+},{
+    timestamps:true,
 })
 
 const User = mongoose.model('User',userSchema)
