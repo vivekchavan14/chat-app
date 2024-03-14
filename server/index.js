9cf75b521e5d12cba4fd6d638cb9673cf338a9a9
vivek <vivekchavan382@gmail.com>
2024-03-15 01:07:24 +0530

    done with backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.route.js'
 import connect from './db/connect.js'
 import cookieParser from 'cookie-parser'
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use('/api/message', messageRoutes)
+app.use('/api/users',userRoutes)
 
 app.listen(port, () => {
     connect()
